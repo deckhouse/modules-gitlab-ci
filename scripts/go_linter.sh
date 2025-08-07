600e2d6ff80668cf21b16918dbc696bc45b6d4dc
Anton Sergunov <anton.sergunov@flant.com>
2025-08-08 01:12:40 +0600

    use origin/main
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

--- scripts/go_linter.sh
+++ scripts/go_linter.sh
@@ -82,7 +82,7 @@ EOF"
 
 if [ -n "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}" ]; then
     git fetch origin ${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}
-    run_linters "modified files" "--new-from-rev=${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}"
+    run_linters "modified files" "--new-from-rev=origin/${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}"
 fi
 
 run_linters "all files"
