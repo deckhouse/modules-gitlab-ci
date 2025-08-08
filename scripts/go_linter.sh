2429c8852b180e4ed8d28ebcdeed8cdab2736cff
Anton Sergunov <anton.sergunov@flant.com>
2025-08-08 21:47:32 +0600

    Changes in go linter script  (#30)
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

--- scripts/go_linter.sh
+++ scripts/go_linter.sh
@@ -14,37 +14,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b . v1.64.5
+unique_index=0
+section_start() {
+    local section_title="${1}"
+    local section_description="${2}"
+    
+    if [ "$GITLAB_CI" == "true" ]; then
+        unique_index=$((unique_index + 1))
+        echo -e "section_start:`date +%s`:${section_title}_${unique_index}[collapsed=true]\r\e[0K${section_description}"
+    else
+        echo "$section_description"
+    fi
+}
+
+section_end() {
+    local section_title="${1}"
+    if [ "$GITLAB_CI" == "true" ]; then
+        echo -e "section_end:`date +%s`:${section_title}_${unique_index}\r\e[0K"
+    fi
+}
+
+linter_version="v1.64.5"
+section_start "install_linter" "Installing golangci-lint@$linter_version"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b . $linter_version
+section_end "install_linter"
 
 basedir=$(pwd)
 failed='false'
 
-for i in $(find images -type f -name go.mod);do
-    dir=$(echo $i | sed 's/go.mod$//')
-    cd $basedir/$dir
-    # check all editions
-    for edition in $GO_BUILD_TAGS ;do
-        echo "Running linter in $dir (edition: $edition)"
-        ../../golangci-lint run --fix --allow-parallel-runners --build-tags $edition
-        if [ $? -ne 0 ]; then
-        echo "Linter failed in $dir (edition: $edition)"
-        failed='true'
-        fi
+run_linters() {
+    local run_for="${1}"
+    local extra_args="${2}"
+    for i in $(find images -type f -name go.mod);do
+        dir=$(echo $i | sed 's/go.mod$//')
+        cd $basedir/$dir
+        # check all editions
+        for edition in $GO_BUILD_TAGS ;do
+            section_start "run_lint" "Running linter in $dir (edition: $edition) for $run_for"
+            ../../golangci-lint run ${extra_args} --fix --color=always --allow-parallel-runners --build-tags $edition
+            local linter_status=$?
+            section_end "run_lint"
+            if [ $linter_status -ne 0 ]; then
+                echo -e "\e[31mLinter FAILED in $dir (edition: $edition) for $run_for\e[0m"
+                failed='true'
+            else
+                echo -e "\e[32mLinter PASSED in $dir (edition: $edition) for $run_for\e[0m"
+            fi
+        done
+
+        cd - > /dev/null
     done
 
-    cd - > /dev/null
-done
 
-rm golangci-lint
-
-if [[ -n "$(git status --porcelain --untracked-files=no)" ]]; then
-    echo "To apply suggested changes run:
-git apply - <<EOF
+    if [[ -n "$(git status --porcelain --untracked-files=no)" ]]; then
+        echo -e "\e[31mLinter requests changes for $run_for\e[0m"
+        section_start "print_patch" "To apply these changes run"
+        echo "git apply - <<EOF
 $(git diff)
 EOF"
-    git checkout -f
-    failed='true'
+        section_end "print_patch" 
+        git checkout -f
+        failed='true'
+    else
+        echo -e "\e[32mLinter doesn't have changes requested for $run_for\e[0m"
+    fi
+}
+
+if [ -n "${CI_MERGE_REQUEST_DIFF_BASE_SHA}" ]; then
+    run_linters "modified files" "--new-from-merge-base=${CI_MERGE_REQUEST_DIFF_BASE_SHA}"
 fi
+
+run_linters "all files"
+
+rm golangci-lint
 
 if [ $failed == 'true' ]; then
     exit 1
